cd43c43c06a254d05f390c919b32923dd507bc1e
anshajk <anshaj.khare2@gmail.com>
2023-04-11 18:40:45 +0530

    Implement timer schedule

--- syncleetcodestats/function_app.py
+++ syncleetcodestats/function_app.py
@@ -2,8 +2,8 @@ import azure.functions
 
 app = azure.functions.FunctionApp()
 
+
 @app.function_name(name="syncleetcodestats")
-@app.route(route="req")
+@app.schedule(schedule="0 */5 * * * *", arg_name="mytimer", run_on_startup=True)
 def main(req: azure.functions.TimerRequest) -> str:
-    user = req.params.get('user')
-    return f'Hello, {user}!'
+    print("Function executed")
